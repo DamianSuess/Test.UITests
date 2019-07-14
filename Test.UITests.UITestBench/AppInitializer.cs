7a5273f365e42c0397486c3da5f629c7eafbe2b4
Damian Suess <damian.suess@xenoinc.com>
2019-07-14 10:09:26 -0400

    base project

---
+++ Test.UITests.UITestBench/AppInitializer.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.UITest;
+
+namespace Test.UITests.UITestBench
+{
+  public class AppInitializer
+  {
+    public static IApp StartApp(Platform platform)
+    {
+      if (platform == Platform.Android)
+      {
+        return ConfigureApp
+          .Android
+          .InstalledApp("com.companyname.Test.UITests")
+          // .ApkFile("C:\work\labs\Test.UITests\.apk");
+          .EnableLocalScreenshots()
+          // .DeviceSerial("emulator-5554") // To run specific device
+          .StartApp();
+      }
+
+      return ConfigureApp.iOS.StartApp();
+    }
+  }
+}
---
+++ Test.UITests.UITestBench/SimpleTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace Test.UITests.UITestBench
+{
+  [TestFixture(Platform.Android)]
+  // [TestFixture(Platform.iOS)]
+  public class SimpleTests
+  {
+    private IApp _app;
+    private Platform _platform;
+
+    public SimpleTests(Platform platform)
+    {
+      this._platform = platform;
+    }
+
+    [SetUp]
+    public void BeforeEachTest()
+    {
+      _app = AppInitializer.StartApp(_platform);
+    }
+
+    [Test]
+    public void WelcomeTextIsDisplayed()
+    {
+      AppResult[] results = _app.WaitForElement(c => c.Marked("Welcome to Xamarin.Forms!"));
+      _app.Screenshot("Welcome screen.");
+
+      Assert.IsTrue(results.Any());
+    }
+
+    [Test]
+    public void InputAndClickTest()
+    {
+      _app.Tap(x => x.Marked("UserNameEntry"));
+
+      _app.EnterText(x => x.Marked("UserNameEntry"), "TestUser");
+      _app.EnterText(x => x.Marked("PasswordEntry"), "testing");
+
+      _app.Tap(x => x.Marked("LoginButton"));
+
+      _app.Screenshot("Tapped login");
+    }
+  }
+}
